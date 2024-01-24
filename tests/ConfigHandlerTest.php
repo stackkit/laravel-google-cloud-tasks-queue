dc54faad120cff5bd44892a16de3aa00ab6c99bd
Andrew Broberg <andrew.broberg@five9.com>
2024-01-24 14:48:52 +1100

    feat: Support path in STACKKIT_CLOUD_TASKS_HANDLER

---
+++ tests/ConfigHandlerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests;
+
+use Stackkit\LaravelGoogleCloudTasksQueue\Config;
+
+class ConfigHandlerTest extends TestCase
+{
+    /**
+     * @dataProvider handlerDataProvider
+     */
+    public function test_it_allows_a_handler_url_to_contain_path(string $handler, string $expectedHandler): void
+    {
+        self::assertSame($expectedHandler, Config::getHandler($handler));
+    }
+
+    public function handlerDataProvider(): array
+    {
+        return [
+            ['https://example.com', 'https://example.com/handle-task'],
+            ['https://example.com/my/path', 'https://example.com/my/path/handle-task'],
+            ['https://example.com/trailing/slashes//', 'https://example.com/trailing/slashes/handle-task'],
+            ['https://example.com/handle-task', 'https://example.com/handle-task'],
+            ['https://example.com/handle-task/', 'https://example.com/handle-task'],
+        ];
+    }
+}
