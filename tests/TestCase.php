c7b88174df158f8f302d7e3e612a902b61e5de0d
Marick van Tuil <info@marickvantuil.nl>
2024-06-26 22:55:12 +0200

    Set the batching db connection

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -47,6 +47,7 @@ class TestCase extends \Orchestra\Testbench\TestCase
     protected function getEnvironmentSetUp($app)
     {
         $app['config']->set('database.default', 'testbench');
+        $app['config']->set('queue.batching.database', 'testbench');
         $port = env('DB_DRIVER') === 'mysql' ? 3307 : 5432;
         $app['config']->set('database.connections.testbench', [
             'driver' => env('DB_DRIVER', 'mysql'),
