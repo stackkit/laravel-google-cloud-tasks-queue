41979e808fd1909317f2a9c6300c5cb86a7f0e4b
Marick van Tuil <marick@Mac.home>
2026-02-27 14:22:33 +0100

    Add queue pause/resume

--- src/CloudTasksServiceProvider.php
+++ src/CloudTasksServiceProvider.php
@@ -7,8 +7,11 @@ namespace Stackkit\LaravelGoogleCloudTasksQueue;
 use Illuminate\Routing\Router;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Queue\QueueManager;
+use Illuminate\Support\Facades\Queue;
 use Illuminate\Foundation\Application;
 use Illuminate\Queue\Events\JobFailed;
+use Illuminate\Queue\Events\QueuePaused;
+use Illuminate\Queue\Events\QueueResumed;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Queue\Events\JobExceptionOccurred;
 use Google\Cloud\Tasks\V2\Client\CloudTasksClient;
@@ -112,6 +115,26 @@ class CloudTasksServiceProvider extends LaravelServiceProvider
             if (! $event->job instanceof CloudTasksJob) {
                 return;
             }
+        });
+
+        $events->listen(QueuePaused::class, function (QueuePaused $event) {
+            $queue = Queue::connection($event->connection);
+
+            if (! $queue instanceof CloudTasksQueue) {
+                return;
+            }
+
+            $queue->pause($event->queue);
+        });
+
+        $events->listen(QueueResumed::class, function (QueueResumed $event) {
+            $queue = Queue::connection($event->connection);
+
+            if (! $queue instanceof CloudTasksQueue) {
+                return;
+            }
+
+            $queue->resume($event->queue);
         });
     }
 
